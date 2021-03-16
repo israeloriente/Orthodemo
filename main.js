914dff645ebec8491fa36af65a9c02eb7d0b4144
Israel <israelnunesoriente@gmail.com>
2021-03-16 12:07:18 +0100

    te amo também

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let win;
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 600, 
+    width: 1000, 
+    minWidth: 1000,
     height: 600,
     backgroundColor: '#ffffff',
     icon: `file://${__dirname}/dist/assets/logo.png`
